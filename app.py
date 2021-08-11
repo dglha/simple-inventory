95a3db38100b55c3bd2d86139fa9a153ce7656ac
Duong Le Ha <duongleha212001@gmail.com>
2021-08-11 20:54:46 +0700

    Add Supplier endpoint

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ from resources.shipper_resource import ShipperInfo, Shippers
 from resources.category_resource import CategoryInfo, Categories
 from resources.customer_resource import CustomerInfo, Customers
 from resources.employee_resource import EmployeeInfo, Employees
-from resources.supplier_resource import SupplierInfo, Suppliers
+from resources.supplier_resource import SupplierInfo, Suppliers, SupplierProduct
 from resources.product_resource import ProductInfo, Products, ProductImage
 from resources.order_resource import Orders, OrderInfo
 
@@ -55,6 +55,7 @@ api.add_resource(EmployeeInfo, '/employee/<int:employee_id>')
 
 api.add_resource(Suppliers, '/supplier')
 api.add_resource(SupplierInfo, '/supplier/<int:supplier_id>')
+api.add_resource(SupplierProduct, '/supplier/<int:supplier_id>/product')
 
 api.add_resource(Products, '/product')
 api.add_resource(ProductInfo, '/product/<int:product_id>')
