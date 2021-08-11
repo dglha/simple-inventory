ed9f81998385373117efdf0ecc23b47a8286d519
Duong Le Ha <duongleha212001@gmail.com>
2021-08-11 20:53:39 +0700

    Hardcode API url

--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
 IMAGEKIT_PRIVATE_KEY = 'private_aqh3Z0CKHn+F9Sv1iFqz5p0d2fg='
 IMAGEKIT_PUBLIC_KEY = 'public_gI7dMWMConcel+EDNsRTd8kG8ww='
 IMAGEKIT_URL = 'https://ik.imagekit.io/digicierge'
+MONGO_BE_URL = 'http://127.0.0.1:8000'
